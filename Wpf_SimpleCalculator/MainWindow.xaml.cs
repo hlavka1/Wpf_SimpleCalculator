76ee89fd08db7ec4eaa2b292db2138217222b708
Justina L Hlavka <justinahlavka@yahoo.com>
2018-10-31 23:13:10 -0400

    yep.. thats right

--- Wpf_SimpleCalculator/MainWindow.xaml.cs
+++ Wpf_SimpleCalculator/MainWindow.xaml.cs
@@ -24,5 +24,46 @@ namespace Wpf_SimpleCalculator
         {
             InitializeComponent();
         }
+
+        private void radBtn_NotDrunk_Checked(object sender, RoutedEventArgs e)
+        {
+            //show not drunk message
+            MessageBox.Show("How boring!");
+        }
+
+        private void radBtn_Drunk_Checked(object sender, RoutedEventArgs e)
+        {
+            //show drunk messge
+           // MessageBox.Show("You should not be drunk calculating Sober up and try again");
+            wnd_DrunkMessage wnd_DrunkMessage = new wnd_DrunkMessage();
+            wnd_DrunkMessage.ShowDialog();
+        }
+
+        private void btn_Calculate_Click(object sender, RoutedEventArgs e)
+        {
+            //if (radBtn_Drunk.Checked == true)
+            //{
+            //    MessageBox.Show("You should not be drunk calculating Sober up and try again");
+
+            //}
+
+            //else
+            //{
+            //    MessageBox.Show("How boring!");
+            //}
+            txtBox_TotalDrunks.Text = (int.Parse(txtBox_DrunksPerBus.Text) * int.Parse(txtBox_NumberOfBuses.Text)).ToString();
+        }
+
+        private void btn_Help_Click(object sender, RoutedEventArgs e)
+        {
+              // MessageBox.Show("Help Me!");
+            wnd_Help wnd_Help = new wnd_Help();
+
+        
+          
+           // wnd_Help.GetWindow(wnd_Help);
+
+            wnd_Help.ShowDialog();
+        }
     }
 }
---
+++ Wpf_SimpleCalculator/wnd_Help.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Wpf_SimpleCalculator
+{
+    /// <summary>
+    /// Interaction logic for wnd_Help.xaml
+    /// </summary>
+    public partial class wnd_Help : Window
+    {
+        public wnd_Help()
+        {
+            InitializeComponent();
+        }
+
+        private void txtBox_Help_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void btn_NotHelpful_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
